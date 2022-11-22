bf46e566ef47832b41b8e85e3cac060265d45932
agustin-y2k <agustin.y2k@gmail.com>
2022-11-22 13:57:01 -0300

    add product service

---
+++ product-service/src/main/java/store/productservice/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package store.productservice.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import store.productservice.model.Product;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+    Product findByName(String name);
+    Product findByCategory(String category);
+    Product deleteByName(String name);
+
+}
